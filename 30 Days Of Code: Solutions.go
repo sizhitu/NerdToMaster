389b34880eec8e368ced267dfa6c49536716d402
sizhitu <sizhitu@126.com>
2022-03-08 09:36:51 +0800

    Update 30 Days Of Code: Solutions.go

--- 30 Days Of Code: Solutions.go
+++ 30 Days Of Code: Solutions.go
@@ -593,3 +593,57 @@ func checkError(err error) {
     }
 }
 
+/*********************************************************************************/
+Day 12: Inheritance （Golang does not have the property, here is python3 solution）
+
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self,firstName,lastName,idNumber,testScore):
+        Person.__init__(Person,firstName,lastName,idNumber)
+        self.testScore=testScore
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        average=sum(self.testScore)/len(self.testScore)
+        if average>=90 and average<=100:
+            return "O"
+        elif average>=80 and average<90:
+            return "E"
+        elif average>=70 and average<80:
+            return "A"
+        elif average>=55 and average<70:
+            return "P"
+        elif average>=40 and average<50:
+            return "D"
+        elif average<40:
+            return "T"
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
