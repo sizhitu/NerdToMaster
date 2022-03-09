10a582894de98b08c3eeb4dd7d2c18a0e3c215b8
sizhitu <sizhitu@126.com>
2022-03-09 13:15:36 +0800

    Update 30 Days Of Code: Solutions.go

--- 30 Days Of Code: Solutions.go
+++ 30 Days Of Code: Solutions.go
@@ -647,3 +647,32 @@ scores = list( map(int, input().split()) )
 s = Student(firstName, lastName, idNum, scores)
 s.printPerson()
 print("Grade:", s.calculate())
+
+/***********************************************************/
+Day 13: Abstract Classes
+
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self, title,author,price):
+            self.price = price
+            super(MyBook,self).__init__(title,author)
+        
+        
+    def display(self):
+        print ("Title: %s" %(self.title))
+        print ("Author: %s" %(self.author))
+        print ("Price: %s" %(self.price))
+        
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
