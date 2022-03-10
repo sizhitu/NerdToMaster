d5f821066bd954edbdd71fae5134f4b1dba6cda6
sizhitu <sizhitu@126.com>
2022-03-10 10:17:34 +0800

    Update 30 Days Of Code: Solutions.go

--- 30 Days Of Code: Solutions.go
+++ 30 Days Of Code: Solutions.go
@@ -676,3 +676,25 @@ author=input()
 price=int(input())
 new_novel=MyBook(title,author,price)
 new_novel.display()
+
+/*******************************************************************************/
+Day 14: Scope
+
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+        self.maximumDifference = None
+
+    def computeDifference(self):
+        self.maximumDifference = max(self.__elements) - min(self.__elements) 
+	# Add your code here
+
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
