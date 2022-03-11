9996f661571f4fc95251983bd71a2b000bbb07f1
sizhitu <sizhitu@126.com>
2022-03-11 11:05:53 +0800

    Update 30 Days Of Code: Solutions.go

--- 30 Days Of Code: Solutions.go
+++ 30 Days Of Code: Solutions.go
@@ -698,3 +698,33 @@ d = Difference(a)
 d.computeDifference()
 
 print(d.maximumDifference)
+
+/***********************************************************************************/
+Day 15: Linked List
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        if head is None:
+            return Node(data)
+        head.next = self.insert(head.next,data)
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
