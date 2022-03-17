d33c27d95c878e392e2501df30720ce1976b3b31
sizhitu <sizhitu@126.com>
2022-03-17 21:44:26 +0800

    Update 30 Days Of Code: Solutions.go

--- 30 Days Of Code: Solutions.go
+++ 30 Days Of Code: Solutions.go
@@ -922,5 +922,50 @@ func checkError(err error) {
 }
 
 /******************************************************************/
+ Day 21: Generics （）
+               
+#include <iostream>
+#include <vector>
+#include <string>
 
-	       
+using namespace std;
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+template<class T>
+
+void printArray(vector<T>arr){
+        for (int i = 0; i < arr.size(); i++) {
+        cout<<arr[i]<<endl;
+    }
+}
+
+int main() {
+	int n;
+	
+	cin >> n;
+	vector<int> int_vector(n);
+	for (int i = 0; i < n; i++) {
+		int value;
+		cin >> value;
+		int_vector[i] = value;
+	}
+	
+	cin >> n;
+	vector<string> string_vector(n);
+	for (int i = 0; i < n; i++) {
+		string value;
+		cin >> value;
+		string_vector[i] = value;
+	}
+
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+
+	return 0;
+}     
