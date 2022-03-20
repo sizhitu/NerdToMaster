6055dc83bfa91bc4e6b07e73ef3a742d68d0f738
sizhitu <sizhitu@126.com>
2022-03-20 10:31:03 +0800

    Create 2D Array_dataStructure.go
    hourglass sum , what confusing me most is the x y axis , should practice more

---
+++ 2D Array_dataStructure.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "math"
+)
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func hourglassSum(arr [][]int32) int32 {
+    // Write your code here
+    var tmp,max int32
+    max = math.MinInt32
+    for x := 1; x < len(arr)-1; x++{
+        for y := 1; y < len(arr[x])-1; y++{
+            //top
+            top := arr[y-1][x-1] + arr[y-1][x] + arr[y-1][x+1]
+            //center
+            center := arr[y][x]
+            //bottom
+            bottom := arr[y+1][x-1] + arr[y+1][x] + arr[y+1][x+1]
+            
+            tmp = top + center + bottom
+            if tmp > max{
+                max = tmp
+            }
+        }
+    }
+    return max
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    var arr [][]int32
+    for i := 0; i < 6; i++ {
+        arrRowTemp := strings.Split(strings.TrimRight(readLine(reader)," \t\r\n"), " ")
+
+        var arrRow []int32
+        for _, arrRowItem := range arrRowTemp {
+            arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+            checkError(err)
+            arrItem := int32(arrItemTemp)
+            arrRow = append(arrRow, arrItem)
+        }
+
+        if len(arrRow) != 6 {
+            panic("Bad input")
+        }
+
+        arr = append(arr, arrRow)
+    }
+
+    result := hourglassSum(arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
