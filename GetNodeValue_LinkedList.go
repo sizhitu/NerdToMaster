6e8376a89e0d5c504e0094b1b32c00ada8f5af91
sizhitu <sizhitu@126.com>
2022-03-30 20:50:11 +0800

    Create GetNodeValue_LinkedList.go
    A start go when B left n step to tail, when B go to the tail, then A is what we want position from tail.

---
+++ GetNodeValue_LinkedList.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+type SinglyLinkedListNode struct {
+    data int32
+    next *SinglyLinkedListNode
+}
+
+type SinglyLinkedList struct {
+    head *SinglyLinkedListNode
+    tail *SinglyLinkedListNode
+}
+
+func (singlyLinkedList *SinglyLinkedList) insertNodeIntoSinglyLinkedList(nodeData int32) {
+    node := &SinglyLinkedListNode {
+        next: nil,
+        data: nodeData,
+    }
+
+    if singlyLinkedList.head == nil {
+        singlyLinkedList.head = node
+    } else {
+        singlyLinkedList.tail.next = node
+    }
+
+    singlyLinkedList.tail = node
+}
+
+func printSinglyLinkedList(node *SinglyLinkedListNode, sep string, writer *bufio.Writer) {
+    for node != nil {
+        fmt.Fprintf(writer, "%d", node.data)
+
+        node = node.next
+
+        if node != nil {
+            fmt.Fprintf(writer, sep)
+        }
+    }
+}
+
+/*
+ * Complete the 'getNode' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER positionFromTail
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data int32
+ *     next *SinglyLinkedListNode
+ * }
+ *
+ */
+
+func getNode(llist *SinglyLinkedListNode, positionFromTail int32) int32 {
+    // Write your code here
+    var tmp *SinglyLinkedListNode
+    tmp = llist
+    for i := 0; llist.next != nil; i++{
+        llist = llist.next
+        if int32(i) >= positionFromTail{
+            tmp = tmp.next
+        } 
+    }
+    return tmp.data
+}
+
+func main() {
